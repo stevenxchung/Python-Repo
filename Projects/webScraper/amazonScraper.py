9fd20103469f717c3e9f6ccc395e362da6b03f91
Steven <stevenxchung@gmail.com>
2018-04-27 01:02:53 -0400

    Fixed major bugs with empty containers

--- Projects/webScraper/amazonScraper.py
+++ Projects/webScraper/amazonScraper.py
@@ -1,9 +1,10 @@
 # Import libraries
+import pdb
 import bs4
 import requests
 from bs4 import BeautifulSoup as soup
 
-my_url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Dgrocery&field-keywords=hot+suace"
+my_url = "https://www.amazon.com/s/ref=sr_pg_2?rh=k%3Ahot+sauce%2Cn%3A16310101%2Cn%3A6502765011&page=2&keywords=hot+sauce&ie=UTF8&qid=1524791074"
 
 # Opens connections and grabs webpage
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
@@ -24,46 +25,81 @@ container = containers[0]
 # f.write(str(container))
 # f.close()
 
-# image_container = container.findAll("img", {"class" : "s-access-image cfMarker"})
+# DEBUG HERE
 
-# # Opening quotations in alt=""
-# indexFirstQ = str(image_container).find("alt") + 4
-# firstChar = indexFirstQ + 1
+# Look in the span
+# span_class = container.findAll("span", {"class" : "a-offscreen"})
+# Get the product price
+# if (span_class[0].text.find("$") != -1):
+#   product_price = span_class[0].text
+# else:
+#   product_price = span_class[1].text
 
-# # Closing quotations in alt=""r
-# indexSecQ = str(image_container).find("\"", indexFirstQ + 1)
+# print(len(containers))
+# print(container)
+# print(span_class)
 
-# print(str(image_container)[firstChar:indexSecQ])
-
-# Write to file
+# # Write to file
 filename = "amazonHotSauces.csv"
-f = open(filename, "a")
-headers = "Product Name, Price\n"
+f = open(filename, "w")
+headers = "Product Name, Price, Reviews\n"
 f.write(headers)
 
-for container in containers:
+room = iter(containers)
+for container in room:
+
+  print("\n##########\n\nElements in container: " + str(len(container)))
+
+  ##### GET PRODUCT NAME #####
+
   # Look in the image container
   image_container = container.findAll("img", {"class" : "s-access-image cfMarker"})
   # Opening quotations in alt=""
   indexFirstQ = str(image_container).find("alt") + 4
   firstChar = indexFirstQ + 1
-  # Closing quotations in alt=""r
+  # Closing quotations in alt=""
   indexSecQ = str(image_container).find("\"", indexFirstQ + 1)
   # Get the product name
   product_name = str(image_container)[firstChar:indexSecQ]
 
+  ##### GET PRODUCT PRICE #####
+
   # Look in the span
   span_class = container.findAll("span", {"class" : "a-offscreen"})
+  print(span_class)
 
-  # Get the product price
-  if (span_class[0].text.find("$") != -1):
-    product_price = span_class[0].text
+  # If span_class is empty, skip a container
+  if not span_class:
+    next(room)
+
+  # When span_class is not empty
   else:
-    product_price = span_class[1].text
+    # Get the product price
+    if (span_class[0].text.find("$") != -1):
+      product_price = span_class[0].text
+    else:
+      product_price = span_class[1].text
+
+  ##### GET PRODUCT REVIEWS #####
+
+  # Look in the a tag containing the number of reviews
+  reviews_a_tag = container.findAll("a", {"class" : "a-size-small a-link-normal a-text-normal"})
+  print("Elements in reviews tag: " + str(len(reviews_a_tag)))
+  print(reviews_a_tag)
+
+  if not reviews_a_tag:
+    next(room)
+
+  else:
+    # Get the number of product reviews
+    product_reviews = reviews_a_tag[0].text.strip()
 
   print("Product Name: " + product_name)
   print("Price " + product_price)
+  print("Product Reviews: " + product_reviews)
 
-  f.write(product_name.replace(",", "-") + "," + product_price + "\n")
+  f.write(product_name.replace(",", "-") + "," + product_price + "," + product_reviews + "\n")
 
 f.close()
+
+input("Done! Press any key to continue")
