33d0ede3a88dafb25a7937755056629542fb7c83
Steven <stevenxchung@gmail.com>
2019-08-20 18:54:51 -0400

    Update 01-adjacent-houses.py

--- Projects/Interview/Real Interviews/01-adjacent-houses.py
+++ Projects/Interview/Real Interviews/01-adjacent-houses.py
@@ -6,7 +6,21 @@
 
 class Solution:
     def cellCompete(self, states, days):
+        flip = {0: 1, 1: 0}
+        end = len(states) - 1
+        # newStates = []
+        for i, state in enumerate(states):
+            # Is first element?
+            if i == 0 and states[1] == 0:
+                states[i] = flip[state]
+            # Is last element?
+            elif i == end and states[end - 1] == 0:
+                states[i] = flip[state]
+            # Meets condition?
+            elif states[i - 1] == states[i + 1]:
+                states[i] = flip[state]
 
+        return states
 
 states = [1, 0, 0, 0, 0, 1, 0, 0]
 days = 1
