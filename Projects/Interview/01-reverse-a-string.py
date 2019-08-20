e97c97f8132d8470b67d898e9be532a449d2aba2
Steven <stevenxchung@gmail.com>
2019-08-19 21:15:45 -0400

    Update 01-reverse-a-string.py

--- Projects/Interview/01-reverse-a-string.py
+++ Projects/Interview/01-reverse-a-string.py
@@ -6,13 +6,20 @@ Follow up: implement iteratively
 
 
 class Solution:
-    def reverseString(self, s):
+    def reverseStringRecursive(self, s):
         if s == '':
             return s
-        print(s[0])
-        return self.reverseString(s[1:]) + s[0]
+        # print(s[0])
+        return self.reverseStringRecursive(s[1:]) + s[0]
+
+    def reverseStringIterative(self, s):
+        newS = ''
+        for c in s:
+            newS = c + newS
+        return newS
 
 
 input = 'Reverse'
 sol = Solution()
-print(sol.reverseString(input))
+print(sol.reverseStringRecursive(input))
+print(sol.reverseStringIterative(input))
