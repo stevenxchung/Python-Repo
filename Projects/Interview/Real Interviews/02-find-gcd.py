6ac5b3549e568072882cfa951d3c68f8c5769895
Steven <stevenxchung@gmail.com>
2019-08-21 18:36:45 -0400

    Create 02-find-gcd.py

---
+++ Projects/Interview/Real Interviews/02-find-gcd.py
@@ -0,0 +1,13 @@
+# Write an algorithm to determine the GCD of N positive integers
+
+
+class Solution:
+    def findGCD(self, n, arr):
+
+
+n = 5
+arr = [2, 3, 4, 5, 6]
+# n = 5
+# arr = [2, 4, 6, 8, 10]
+sol = Solution()
+print(sol.findGCD(n, arr))
