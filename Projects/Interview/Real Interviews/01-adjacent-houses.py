382ebc8948433106fd5fe4820bf2e74c5a0acb61
Steven <stevenxchung@gmail.com>
2019-08-21 18:26:20 -0400

    Update 01-adjacent-houses.py

--- Projects/Interview/Real Interviews/01-adjacent-houses.py
+++ Projects/Interview/Real Interviews/01-adjacent-houses.py
@@ -1,28 +1,40 @@
 # Leet 957
 
-# If neighbors on both sides are opposite value then flip
+# If neighbors on both sides match then inactive(0)
+# Otherwise, active (1)
 # Assume neighbors on ends are inactive (0)
 
 
 class Solution:
     def cellCompete(self, states, days):
-        flip = {0: 1, 1: 0}
         end = len(states) - 1
-        # newStates = []
-        for i, state in enumerate(states):
-            # Is first element?
-            if i == 0 and states[1] == 0:
-                states[i] = flip[state]
-            # Is last element?
-            elif i == end and states[end - 1] == 0:
-                states[i] = flip[state]
-            # Meets condition?
-            elif states[i - 1] == states[i + 1]:
-                states[i] = flip[state]
+        prev = states
+        while days:
+          days -= 1
+          new = []
+          for i, state in enumerate(prev):
+              # Is first element?
+              if i == 0 and prev[1] == 0:
+                  new.append(0)
+                  # states[i] = flip[state]
+              # Is last element?
+              elif i == end and prev[end - 1] == 0:
+                  new.append(0)
+                  # states[i] = flip[state]
+              # Meets condition?
+              elif (i != 0 and i != 7) and prev[i - 1] == prev[i + 1]:
+                  new.append(0)
+                  # states[i] = flip[state]
+              else:
+                  new.append(1)
+          # Store new states as previous for next iteration
+          prev = new
 
-        return states
+        return new
 
-states = [1, 0, 0, 0, 0, 1, 0, 0]
-days = 1
+# states = [1, 0, 0, 0, 0, 1, 0, 0]
+# days = 1
+states = [1, 1, 1, 0, 1, 1, 1, 1]
+days = 2
 sol = Solution()
 print(sol.cellCompete(states, days))
