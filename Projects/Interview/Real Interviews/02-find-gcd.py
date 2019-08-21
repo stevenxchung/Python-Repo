dd3a493a2f0017db6a1c527adba9270ec337009c
Steven <stevenxchung@gmail.com>
2019-08-21 19:08:03 -0400

    Update 02-find-gcd.py

--- Projects/Interview/Real Interviews/02-find-gcd.py
+++ Projects/Interview/Real Interviews/02-find-gcd.py
@@ -3,11 +3,19 @@
 
 class Solution:
     def findGCD(self, n, arr):
+        # Assume sorted
+        gcd = arr[0]
+        for i, num in enumerate(arr):
+          # If an element has remainder, return 1
+          if num % gcd != 0:
+            return 1
+        # Otherwise, the first element is the GCD
+        return gcd
 
 
-n = 5
-arr = [2, 3, 4, 5, 6]
 # n = 5
-# arr = [2, 4, 6, 8, 10]
+# arr = [2, 3, 4, 5, 6]
+n = 5
+arr = [2, 4, 6, 8, 10]
 sol = Solution()
 print(sol.findGCD(n, arr))
