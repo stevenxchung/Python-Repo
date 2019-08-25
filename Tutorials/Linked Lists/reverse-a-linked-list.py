516002522f0b2553fad01fba572c31b90d150e39
Steven <stevenxchung@gmail.com>
2019-08-25 18:26:17 -0400

    Create reverse-a-linked-list.py

---
+++ Tutorials/Linked Lists/reverse-a-linked-list.py
@@ -0,0 +1,17 @@
+# Reverse a linked list
+
+
+class Node:
+
+class LinkedList:
+
+# Building a linked list
+ll = LinkedList()
+ll.addNode(1)
+ll.addNode(2)
+ll.addNode(3)
+ll.printLinkedList()
+
+# Reverse a linked list
+ll.reverse()
+ll.printLinkedList()
