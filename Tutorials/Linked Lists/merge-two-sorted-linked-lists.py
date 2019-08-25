3b4eed4484ba12b69789a3e9e8e0f8b55f0aeecc
Steven <stevenxchung@gmail.com>
2019-08-25 09:58:36 -0400

    Create merge-two-sorted-linked-lists.py

---
+++ Tutorials/Linked Lists/merge-two-sorted-linked-lists.py
@@ -0,0 +1,53 @@
+# Merge two sorted linked lists (from leet code)
+
+
+class Node:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.nextNode = nextNode
+
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    # Adds node to the end of the list
+    def addNode(self, value):
+        node = Node(value)
+        if self.head == None:
+            self.head = node
+            return
+
+        # Start at the head node
+        currentNode = self.head
+        while True:
+            if currentNode.nextNode == None:
+                currentNode.nextNode = node
+                break
+            # Traverse through linked list
+            currentNode = currentNode.nextNode
+
+    def printLinkedList(self):
+        currentNode = self.head
+        while currentNode:
+            print(currentNode.value, "->", end=" ")
+            currentNode = currentNode.nextNode
+        print("None")
+
+
+# Build lists
+l1 = LinkedList()
+l2 = LinkedList()
+l1.addNode(1)
+l1.addNode(5)
+l1.addNode(7)
+l2.addNode(2)
+l2.addNode(4)
+l2.addNode(6)
+l1.printLinkedList()
+l2.printLinkedList()
+
+# Solution
+# l3 = linkedList()
+# l3.mergeTwoSorted(l1, l2)
+# l3.printLinkedList()
