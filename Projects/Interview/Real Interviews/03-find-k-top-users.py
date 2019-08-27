4300fa85e6d1875a21437fbc1b86c3f34099621d
Steven <stevenxchung@gmail.com>
2019-08-27 14:55:20 -0400

    Create 03-find-k-top-users.py

---
+++ Projects/Interview/Real Interviews/03-find-k-top-users.py
@@ -0,0 +1,12 @@
+'''
+Find the top K active users from AWS service log.
+
+Log sample:
+ID | Activity
+0001 | service A, service B, service C
+0002 | service A, service C
+0001 | service C
+0003 | service A
+
+Top two users would yield user 0001 with 4 service calls and user 0002 with 2 service calls
+'''
