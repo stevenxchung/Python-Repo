3be1686de31dc7cbeeb9018efed97d4e17be243c
Steven <stevenxchung@gmail.com>
2019-08-29 19:25:17 -0400

    Create 01-convert-fib-to-iterative-sol.py

---
+++ Tutorials/Recursion/01-convert-fib-to-iterative-sol.py
@@ -0,0 +1,16 @@
+'''
+Convert the Fibonacci function from recursive to iterative
+'''
+
+
+class Solution:
+    def fibonacci(self, n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return self.fibonacci(n - 1) + self.fibonacci(n - 2)
+
+
+s = Solution()
+print(s.fibonacci(10))
