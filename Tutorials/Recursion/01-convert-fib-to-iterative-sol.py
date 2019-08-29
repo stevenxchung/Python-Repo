b4341a3501340bccc02689a38dd153421e5de5e1
Steven <stevenxchung@gmail.com>
2019-08-29 19:40:43 -0400

    Update 01-convert-fib-to-iterative-sol.py

--- Tutorials/Recursion/01-convert-fib-to-iterative-sol.py
+++ Tutorials/Recursion/01-convert-fib-to-iterative-sol.py
@@ -11,6 +11,22 @@ class Solution:
             return 1
         return self.fibonacci(n - 1) + self.fibonacci(n - 2)
 
+    def iterativeFib(self, n):
+        stack = []
+        stack.append(n)
+        total = 0
+        while len(stack) > 0:
+            n = stack.pop()
+            if n == 0:
+                total += 0
+            elif n == 1:
+                total += 1
+            else:
+                stack.append(n - 1)
+                stack.append(n - 2)
+        return total
+
 
 s = Solution()
-print(s.fibonacci(10))
+# print(s.fibonacci(10))
+print(s.iterativeFib(10))
