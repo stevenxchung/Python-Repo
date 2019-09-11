e9eb8653ec16524c60a8abe9698413fb4dcb1466
Steven <stevenxchung@gmail.com>
2019-09-11 19:14:52 -0400

    Update dfs-and-bfs.py

--- Tutorials/Trees and Graphs/dfs-and-bfs.py
+++ Tutorials/Trees and Graphs/dfs-and-bfs.py
@@ -4,10 +4,12 @@ class Node(object):
         self.children = children
 
     def dfs(self):
+        print('DFS Recursive: ', end='')
         self._dfsHelper(self)
+        print()
 
     def _dfsHelper(self, node):
-        print(node.data)
+        print(node.data, end=' ')
         if node.children != None:
             for child in node.children:
                 self._dfsHelper(child)
@@ -15,21 +17,25 @@ class Node(object):
     def dfsIterative(self):
         stack = []
         stack.append(self)
+        print('DFS Iterative: ', end='')
         while len(stack) > 0:
             node = stack.pop()
-            print(node.data)
+            print(node.data, end=' ')
             if node.children != None:
                 stack += reversed(node.children)
+        print()
 
     def bfs(self):
         queue = []
         queue.append(self)
+        print('BFS: ', end='')
         while len(queue) > 0:
             node = queue.pop(0)
-            print(node.data)
+            print(node.data, end=' ')
             if node.children != None:
                 for child in node.children:
                     queue.append(child)
+        print()
 
 
 a = Node('a')
@@ -40,3 +46,6 @@ e = Node('e')
 f = Node('f')
 a.children = [b, c, d]
 b.children = [e, f]
+a.dfs()
+a.dfsIterative()
+a.bfs()
