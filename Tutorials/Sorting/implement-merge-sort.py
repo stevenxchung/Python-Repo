56154b14aec7758888e98603275a34de72092597
Steven <stevenxchung@gmail.com>
2019-09-27 19:43:05 -0400

    Create implement-merge-sort.py

---
+++ Tutorials/Sorting/implement-merge-sort.py
@@ -0,0 +1,12 @@
+'''
+Given an array of random numbers use merge sort to sort array from least to greatest
+'''
+
+
+class Solution:
+    def mergeSort(self, arr):
+      
+# Test merge sort
+test = [5, 9, 0, 7, 3]
+sol = Solution()
+sol.mergeSort(test)
