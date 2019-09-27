ca59c6c3b29531ab21f3a186f295da137b7f338c
Steven <stevenxchung@gmail.com>
2019-09-27 19:43:13 -0400

    Update implement-merge-sort.py

--- Tutorials/Sorting/implement-merge-sort.py
+++ Tutorials/Sorting/implement-merge-sort.py
@@ -5,7 +5,38 @@ Given an array of random numbers use merge sort to sort array from least to grea
 
 class Solution:
     def mergeSort(self, arr):
-      
+        if len(arr) > 1:
+            mid = len(arr) // 2
+            L = arr[:mid]
+            R = arr[mid:]
+
+            self.mergeSort(L)
+            self.mergeSort(R)
+
+            i = j = k = 0
+
+            while i < len(L) and j < len(R):
+                if L[i] < R[j]:
+                    arr[k] = L[i]
+                    i += 1
+                else:
+                    arr[k] = R[j]
+                    j += 1
+                k += 1
+
+            while i < len(L):
+                arr[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < len(R):
+                arr[k] = R[j]
+                j += 1
+                k += 1
+
+        print('Merging:', arr)
+
+
 # Test merge sort
 test = [5, 9, 0, 7, 3]
 sol = Solution()
