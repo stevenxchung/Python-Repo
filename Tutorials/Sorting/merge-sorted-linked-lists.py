70860a3c064941ea7cba29a0b4ac24b2a7a43745
Steven <stevenxchung@gmail.com>
2019-09-28 10:38:56 -0400

    Update merge-sorted-linked-lists.py

--- Tutorials/Sorting/merge-sorted-linked-lists.py
+++ Tutorials/Sorting/merge-sorted-linked-lists.py
@@ -12,3 +12,31 @@ class Node:
 class linkedList:
     def __init__(self, head=None):
         self.head = head
+
+    def insert(self, data):
+        node = Node(data)
+        if self.head is None:
+            self.head = node
+            return
+
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = node
+
+    def printLinkedList(self):
+        current = self.head
+        while current:
+            print(current.data, '->', end=' ')
+            current = current.next
+        print('None')
+
+
+ll = linkedList()
+ll.printLinkedList()
+ll.insert(5)
+ll.printLinkedList()
+ll.insert(10)
+ll.printLinkedList()
+ll.insert(15)
+ll.printLinkedList()
