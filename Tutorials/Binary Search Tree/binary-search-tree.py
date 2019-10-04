7feda70e6ff43c87275d385a9298e07b05efc445
Steven <stevenxchung@gmail.com>
2019-10-04 16:31:53 -0400

    Update binary-search-tree.py

--- Tutorials/Binary Search Tree/binary-search-tree.py
+++ Tutorials/Binary Search Tree/binary-search-tree.py
@@ -2,12 +2,21 @@
 Implement a binary search tree
 '''
 
+class Node:
+  def __init__(self, val=None, next=None):
+    self.val = val
+    self.next = next
 
 class BinaryTree:
     def search(self, root, key):
-      # Code here
+      if root is None or root.val == key:
+        return root
+      if root.val < key:
+        return search(root.right, key)
+      else:
+        return search(root.left, key)
 
     def binarySearch(self, arr, l, r, x):
       # Code here
 
-# Test
+# Test binary search
