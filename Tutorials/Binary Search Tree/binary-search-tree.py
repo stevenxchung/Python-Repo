b4483c30ca6f2cd0f046cec8056331bcebb6c91f
Steven <stevenxchung@gmail.com>
2019-10-04 16:29:22 -0400

    Create binary-search-tree.py

---
+++ Tutorials/Binary Search Tree/binary-search-tree.py
@@ -0,0 +1,13 @@
+'''
+Implement a binary search tree
+'''
+
+
+class BinaryTree:
+    def search(self, root, key):
+      # Code here
+
+    def binarySearch(self, arr, l, r, x):
+      # Code here
+
+# Test
