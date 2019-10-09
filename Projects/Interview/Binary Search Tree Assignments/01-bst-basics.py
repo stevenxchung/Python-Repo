e2e69a16b183293962b40a1f40c3dbf3bbf4fb6e
Steven <stevenxchung@gmail.com>
2019-10-09 15:02:52 -0400

    Create 01-bst-basics.py

---
+++ Projects/Interview/Binary Search Tree Assignments/01-bst-basics.py
@@ -0,0 +1,14 @@
+'''
+A BST (binary search tree) is a binary tree where:
+  a. The value in each node to its left subtree is less than the current node
+  b. The value in each node to its right subtree is greater than the current node
+
+Looking up a value takes O(log(n)) if the tree is balanced and O(h) is the height of the tree
+
+Implement the following:
+1. Search in a BST
+2. Insert into a BST
+3. Delete Node in a BST
+'''
+
+# Test
