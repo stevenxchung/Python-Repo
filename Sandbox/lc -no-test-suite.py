e8f9836eafae820beb78dedde303bc425dd7e783
Steven <stevenxchung@gmail.com>
2022-12-12 21:20:05 -0500

    LC 155

--- Sandbox/lc -no-test-suite.py
+++ Sandbox/lc -no-test-suite.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import heapq
+from math import inf
 from time import time
 from typing import List
 
@@ -18,49 +19,44 @@ class Node:
 
 class Solution:
     def __init__(self, debug=False):
-        self.table = defaultdict(list)
+        self.stack = [] # (val, min_val)
+        self.min = inf
         self.debug = debug
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        self.table[key].append((value, timestamp))
 
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.table:
-            if self.debug:
-                print('Nothing!')
-            return ''
+    def push(self, val: int) -> None:
+        if val < self.min:
+            self.min = val
+        self.stack.append((val, self.min))
 
-        arr = self.table[key]
-        l, r = 0, len(arr) - 1
-        while l <= r:
-            m = (l + r) // 2
-            if arr[m][-1] == timestamp:
-                if self.debug:
-                    print(arr[m][0])
-                return arr[m][0]
-            elif arr[m][-1] < timestamp:
-                l = m + 1
-            else:
-                r = m - 1
+        
 
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        res = self.stack[-1][0]
         if self.debug:
-            print('Previous:', arr[m][0])
-        return arr[m][0]
+            print(res)
+        return res
+        
+
+    def getMin(self) -> int:
+        res = self.stack[-1][-1]
+        if self.debug:
+            print(res)
+        return res
 
 
 if __name__ == '__main__':
     test = Solution(debug=True)
     sol_start = time()
-    # Store the key 'foo' and value 'bar' along with timestamp = 1.
-    test.set('foo', 'bar', 1)
-    # Return 'bar'
-    test.get('foo', 1)
-    # Return 'bar', since there is no value corresponding to 'foo' at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is 'bar'.
-    test.get('foo', 3)
-    # Store the key 'foo' and value 'bar2' along with timestamp = 4.
-    test.set('foo', 'bar2', 4)
-    # Return 'bar2'
-    test.get('foo', 4)
-    # Return 'bar2'
-    test.get('foo', 5)
+    test.push(-2)
+    test.push(0)
+    test.push(-3)
+    test.getMin()  # return -3
+    test.pop()
+    test.top()    # return 0
+    test.getMin()  # return -2
     print(f'Runtime for our solution: {time() - sol_start}')
