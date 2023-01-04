9a5d3ebdaab166b4ab08ab0e414f81859124f57d
Steven <stevenxchung@gmail.com>
2023-01-04 11:20:07 -0500

    LC 981

--- Sandbox/lc-no-test-suite.py
+++ Sandbox/lc-no-test-suite.py
@@ -19,66 +19,42 @@ class Node:
 
 
 class Solution:
-    def __init__(self, capacity: int, debug=False):
+    def __init__(self, debug=False):
+        self.table = defaultdict(list)  # {key: (timestamp, value)}
         self.debug = debug
-        self.capacity = capacity
-        self.cache = {}  # {key: Node()}
-        # LRU is head and MRU is tail
-        self.lru, self.mru = Node(), Node()
-        self.lru.next, self.mru.prev = self.mru, self.lru
 
-    def delete(self, node: Node):
-        # Delete by linking neighbors
-        l, r = node.prev, node.next
-        l.next, r.prev = r, l
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.table[key].append((timestamp, value))
 
-    def insert(self, node: Node):
-        # Every element is added to tail (MRU)
-        l, r = self.mru.prev, self.mru
-        l.next = r.prev = node
-        node.prev, node.next = l, r
-
-    def get(self, key: int) -> int:
-        res = -1
-        if key in self.cache:
-            # Move to tail
-            self.delete(self.cache[key])
-            self.insert(self.cache[key])
-            res = key
+    def get(self, key: str, timestamp: int) -> str:
+        res, arr = '', self.table.get(key, [])
+        l, r = 0, len(arr) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if timestamp >= arr[mid][0]:
+                res = arr[mid][-1]
+                l = mid + 1
+            else:
+                r = mid - 1
 
         if self.debug:
             print(res)
         return res
 
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.delete(self.cache[key])
-
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-
-        if len(self.cache) > self.capacity:
-            # Over capacity, remove LRU
-            lru = self.lru.next
-            self.delete(self.cache[lru.key])
-            if self.debug:
-                print(lru.key)
-            del self.cache[lru.key]
-
-        if self.debug:
-            print(self.cache.keys())
-
 
 if __name__ == '__main__':
-    test = Solution(2, debug=True)
+    test = Solution(debug=True)
     sol_start = time()
-    test.put(1, 1)  # Cache is {1=1}
-    test.put(2, 2)  # Cache is {1=1, 2=2}
-    test.get(1)  # Return 1
-    test.put(3, 3)  # LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-    test.get(2)  # Returns -1 (not found)
-    test.put(4, 4)  # LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-    test.get(1)  # Return -1 (not found)
-    test.get(3)  # Return 3
-    test.get(4)  # Return 4
+    # Store the key 'foo' and value 'bar' along with timestamp = 1.
+    test.set('foo', 'bar', 1)
+    # Return 'bar'
+    test.get('foo', 1)
+    # Return 'bar', since there is no value corresponding to 'foo' at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is 'bar'.
+    test.get('foo', 3)
+    # Store the key 'foo' and value 'bar2' along with timestamp = 4.
+    test.set('foo', 'bar2', 4)
+    # Return 'bar2'
+    test.get('foo', 4)
+    # Return 'bar2'
+    test.get('foo', 5)
     print(f'Runtime for our solution: {time() - sol_start}')
