5f62c8cd29ff531abbdc995e51da4973c473ba29
Steven <stevenxchung@gmail.com>
2023-02-06 21:48:34 -0500

    Update fibonacci-patterns.py

--- Sandbox/fibonacci-patterns.py
+++ Sandbox/fibonacci-patterns.py
@@ -3,61 +3,91 @@ from time import time
 
 
 class Solution:
-    def fib(self, n):
-        if n < 3:
-            return 1
-        else:
-            return self.fib(n - 1) + self.fib(n - 2)
+    def __init__(self, debug=False) -> None:
+        self.debug = debug
 
-    def fib_top_down_dp(self, n, memo={}):
-        if n in memo:
-            return memo[n]
-        if n < 3:
-            return 1
-        else:
-            res = self.fib_top_down_dp(n - 1) + self.fib_top_down_dp(n - 2)
-            memo[n] = res
-            return res
+    def fib(self, n):
+        if n <= 1:
+            return n
+
+        return self.fib(n - 1) + self.fib(n - 2)
+
+    def fib_top_down_dp(self, n, cache={}):
+        if n <= 1:
+            return n
+        if n in cache:
+            return cache[n]
+
+        cache[n] = self.fib_top_down_dp(n - 1) + self.fib_top_down_dp(n - 2)
+        return cache[n]
 
     def fib_bottom_up_dp(self, n):
-        if n < 3:
-            return 1
-        res = [0] * (n + 1)
-        res[1], res[2] = 1, 1
+        if n <= 1:
+            return n
+
+        # Initialize cache
+        cache = [0] * (n + 1)
+        cache[1], cache[2] = 1, 1
+
+        # Will not need to loop until n = 3
         for i in range(3, n + 1):
-            res[i] = res[i - 1] + res[i - 2]
-        return res[n]
+            cache[i] = cache[i - 1] + cache[i - 2]
 
-    def quantify(self, test_cases, runs=100000):
-        sol_start = time()
-        for i in range(runs):
-            for case in test_cases:
-                if i == 0:
-                    print(self.fib(case))
-                else:
-                    self.fib(case)
-        print(f'Runtime for our solution: {time() - sol_start}')
+        return cache[n]
+
+    def fib_bottom_up_dp_neetcode(self, n):
+        if n < 2:
+            return n
+
+        cache = [0, 1]
+        i = 2
+        while i <= n:
+            temp = cache[1]
+            cache[1] = cache[0] + cache[1]
+            cache[0] = temp
+            i += 1
+
+        return cache[1]
+
+    def quantify(self, test_cases, runs=50000):
+        # sol_start = time()
+        # for i in range(runs):
+        #     for case in test_cases:
+        #         if i == 0 and self.debug:
+        #             print(self.fib(case))
+        #         else:
+        #             self.fib(case)
+        # print(f'Runtime for our solution: {time() - sol_start}')
+
+        # ref_start = time()
+        # for i in range(0, runs):
+        #     for case in test_cases:
+        #         if i == 0 and self.debug:
+        #             print(self.fib_top_down_dp(case))
+        #         else:
+        #             self.fib_top_down_dp(case)
+        # print(f'Runtime for reference: {time() - ref_start}')
 
         ref_start = time()
         for i in range(0, runs):
             for case in test_cases:
-                if i == 0:
-                    print(self.fib_top_down_dp(case))
-                else:
-                    self.fib_top_down_dp(case)
-        print(f'Runtime for reference: {time() - ref_start}')
-
-        ref_start = time()
-        for i in range(0, runs):
-            for case in test_cases:
-                if i == 0:
+                if i == 0 and self.debug:
                     print(self.fib_bottom_up_dp(case))
                 else:
                     self.fib_bottom_up_dp(case)
         print(f'Runtime for reference: {time() - ref_start}')
 
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0 and self.debug:
+                    print(self.fib_bottom_up_dp_neetcode(case))
+                else:
+                    self.fib_bottom_up_dp_neetcode(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
 
 if __name__ == '__main__':
-    test = Solution()
-    test_cases = [n for n in range(1, 9)]
+    test = Solution(debug=False)
+    test_cases = [n for n in range(20)]
     test.quantify(test_cases)
