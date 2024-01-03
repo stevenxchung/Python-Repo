6426e2cc4f083888c038938df5c5006e5d70f75d
Steven <stevenxchung@gmail.com>
2024-01-02 21:18:54 -0500

    Update 68-text-justification.py

--- Prep/Interviews/68-text-justification.py
+++ Prep/Interviews/68-text-justification.py
@@ -73,7 +73,7 @@ class Solution:
         for i in range(len(lines)):
             while spaces_per_line[i] > 0:
                 if len(lines[i]) == 1:
-                    # THere is only one character in the line
+                    # There is only one character in the line
                     lines[i][-1] = lines[i][-1] + ' ' * spaces_per_line[i]
                     spaces_per_line[i] = 0
                 else:
@@ -93,7 +93,36 @@ class Solution:
         return res
 
     def reference(self, words: List[str], maxWidth: int) -> List[str]:
-        return
+        result, current_list, num_of_letters = [], [], 0
+        # result -> stores final result output
+        # current_list -> stores list of words which are traversed but not yet added to result
+        # num_of_letters -> stores number of chars corresponding to words in current_list
+        for word in words:
+            # total no. of chars in current_list + total no. of chars in current word
+            # + total no. of words ~= min. number of spaces between words
+            if num_of_letters + len(word) + len(current_list) > maxWidth:
+                # size will be used for module "magic" for round robin
+                # we use max. 1 because atleast one word would be there and to avoid modulo by 0
+                size = max(1, len(current_list) - 1)
+
+                for i in range(maxWidth - num_of_letters):
+                    # add space to each word in round robin fashion
+                    index = i % size
+                    current_list[index] += ' '
+
+                # add current line of words to the output
+                result.append("".join(current_list))
+                current_list, num_of_letters = [], 0
+
+            # add current word to the list and add length to char count
+            current_list.append(word)
+            num_of_letters += len(word)
+
+        # form last line by join with space and left justify to maxWidth using ljust (python method)
+        # that means pad additional spaces to the right to make string length equal to maxWidth
+        result.append(" ".join(current_list).ljust(maxWidth))
+
+        return result
 
     def quantify(self, test_cases, runs=50000):
         sol_start = time()
@@ -104,13 +133,14 @@ class Solution:
                     print('\n')
                 else:
                     self.fullJustify(*case)
-        print(f'Runtime for our solution: {time() - sol_start}')
+        print(f'Runtime for our solution: {time() - sol_start}\n')
 
         ref_start = time()
         for i in range(0, runs):
             for case in test_cases:
                 if i == 0:
-                    print(self.reference(*case))
+                    print(*self.reference(*case), sep='\n')
+                    print('\n')
                 else:
                     self.reference(*case)
         print(f'Runtime for reference: {time() - ref_start}')
