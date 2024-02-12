e03a8027391a7842fd01b3c4937877afbededcd0
Steven <stevenxchung@gmail.com>
2024-02-12 10:28:56 -0500

    LC 33

--- Sandbox/lc-with-test-suite.py
+++ Sandbox/lc-with-test-suite.py
@@ -43,23 +43,29 @@ class Node:
 
 
 class Solution:
-    def test(self, nums: List[int]) -> int:
+    def test(self, nums: List[int], target: int) -> int:
         '''
-        - Given array is sorted and nums are unique
-        - Compare nums[m] and nums[r]
-        - Binary search remaining segment
+        - Given sorted array of distinct values
+        - Compare nums[l] to nums[r] to check for rotation
+        - Binary search depending on which section target
         '''
         l, r = 0, len(nums) - 1
         while l < r:
             m = l + (r - l) // 2
-            if nums[m] < nums[r]:
-                # Min could be at m
-                r = m
+            if nums[l] <= nums[m]:
+                # Left sorted
+                if nums[l] <= target <= nums[m]:
+                    r = m
+                else:
+                    l = m + 1
             else:
-                # Min must be to the right of m
-                l = m + 1
+                # Right sorted
+                if nums[m] < target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m
 
-        return nums[l]
+        return l if nums[l] == target else -1
 
     def reference(self):
         return
@@ -69,9 +75,9 @@ class Solution:
         for i in range(runs):
             for case in test_cases:
                 if i == 0:
-                    print(self.test(case))
+                    print(self.test(*case))
                 else:
-                    self.test(case)
+                    self.test(*case)
         print(f'Runtime for our solution: {time() - sol_start}\n')
 
         # ref_start = time()
@@ -87,10 +93,12 @@ class Solution:
 if __name__ == '__main__':
     test = Solution()
     test_cases = [
-        [3, 4, 5, 1, 2],
-        [4, 5, 6, 7, 0, 1, 2],
-        [11, 13, 15, 17],
+        ([4, 5, 6, 7, 0, 1, 2], 0),
+        ([4, 5, 6, 7, 0, 1, 2], 3),
+        ([1], 0),
         # Additional
-        [4, 5, 1, 2, 3],
+        ([3, 1], 3),
+        ([5, 1, 3], 3),
+        ([5, 1, 3], 1),
     ]
     test.quantify(test_cases)
