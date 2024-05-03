a401cdb5b52248bb6138dc4dc5a6e4350925c9b9
Steven <stevenxchung@gmail.com>
2024-05-02 21:04:23 -0400

    Update 04CP-find-odd-one.py

--- Prep/Interviews/Custom/04CP-find-odd-one.py
+++ Prep/Interviews/Custom/04CP-find-odd-one.py
@@ -1,30 +1,28 @@
 '''
+Original: https://leetcode.com/problems/odd-string-difference/description/
+
 Given a series of strings, find the string where the distances between each neighboring character in the string is different from the rest of the strings.
 '''
+
+from collections import Counter
 from time import time
 from typing import List
 
 
 class Solution:
     def find_odd_one(self, series: List[str]) -> str:
-        counts = {}
-        dists = {}
+        count_map, res = Counter(), {}
         for s in series:
-            i, j = 0, 1
             char_dist = []
-            while j < len(s):
-                char_dist.append(ord(s[i]) - ord(s[j]))
-                i += 1
-                j += 1
+            for i in range(1, len(s)):
+                char_dist.append(ord(s[i]) - ord(s[i - 1]))
 
-            if str(char_dist) not in counts:
-                counts[str(char_dist)] = 1
-                dists[str(char_dist)] = s
-            else:
-                counts[str(char_dist)] += 1
+            # Increment counter if necessary and add word to result map
+            count_map[str(char_dist)] += 1
+            res[str(char_dist)] = s
 
         # The key with the minimum count is the odd one
-        odd_one = dists[min(counts, key=counts.get)]
+        odd_one = res[min(count_map, key=count_map.get)]
 
         return odd_one
 
