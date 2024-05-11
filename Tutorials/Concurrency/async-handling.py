816d821876358f9185a02dacfca3f7dd2caab09f
Steven <stevenxchung@gmail.com>
2024-05-11 07:59:33 -0400

    Clean-up concurrency handling and rename files

--- Tutorials/Concurrency/async-handling.py
+++ Tutorials/Concurrency/async-handling.py
@@ -5,31 +5,21 @@ import time
 
 
 class Solution:
-    def __init__(self, debug=False) -> None:
+    def __init__(self, t_sleep=0.1, debug=False) -> None:
         self.debug = debug
+        self.t_sleep = t_sleep
         self.res = []
 
     async def fetch_data(self, n: int):
         if self.debug:
             print('Fetching...')
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(self.t_sleep)
         if self.debug:
             print('Done fetching!')
 
         return random.choice([{f'data {n}': f'test {n}'}, [], [Decimal('999')]])
 
-    async def print_numbers(self, n: int):
-        for i in range(n):
-            if self.debug:
-                print(i)
-                await asyncio.sleep(0.5)
-
     async def test(self, n: int) -> str:
-        # Calls and waits one at a time
-        # for i in range(n):
-        #     data = await self.fetch_data(i)
-        #     self.res.append(data)
-
         # Calls but gathers aggregated results from coroutine
         tasks = [self.fetch_data(i) for i in range(n)]
         self.res = await asyncio.gather(*tasks)
@@ -39,31 +29,34 @@ class Solution:
 
         return 'Done'
 
-    def reference():
-        return
+    def reference(self, n: int):
+        for i in range(n):
+            if self.debug:
+                print(i)
+            time.sleep(self.t_sleep)
 
     def quantify(self, test_cases, runs=1):
         sol_start = time.time()
         for i in range(runs):
             for case in test_cases:
-                loop = asyncio.run(self.test(case))
+                res = asyncio.run(self.test(case))
                 if i == 0:
-                    print(loop)
+                    print(res)
                 else:
-                    loop
+                    res
         print(f'Runtime for our solution: {time.time() - sol_start}')
 
-        # ref_start = time.time()
-        # for i in range(0, runs):
-        #     for case in test_cases:
-        #         if i == 0:
-        #             print(self.reference(case))
-        #         else:
-        #             self.reference(case)
-        # print(f'Runtime for reference: {time.time() - ref_start}')
+        ref_start = time.time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time.time() - ref_start}')
 
 
 if __name__ == '__main__':
-    test = Solution(debug=True)
+    test = Solution(debug=False)
     test_cases = [10, 9, 8, 7, 6]
     test.quantify(test_cases)
---
+++ Tutorials/Concurrency/threading.py
@@ -0,0 +1,79 @@
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
+from decimal import Decimal
+import random
+import time
+
+
+class Solution:
+    def __init__(self, t_sleep=0.1, debug=False) -> None:
+        self.debug = debug
+        self.t_sleep = t_sleep
+        self.res = []
+
+    def fetch_data(self, n: int):
+        if self.debug:
+            print('Fetching...')
+        # Simulate a synchronous delay
+        time.sleep(self.t_sleep)
+        if self.debug:
+            print('Done fetching!')
+
+        return random.choice([{f'data {n}': f'test {n}'}, [], [Decimal('999')]])
+
+    def print_numbers(self, n: int):
+        for i in range(n):
+            if self.debug:
+                print(i)
+            # Simulate a synchronous delay
+            time.sleep(self.t_sleep)
+
+    async def to_thread(self, func, *args):
+        # Required for threading synchronous functions
+        with ThreadPoolExecutor() as pool:
+            return await asyncio.get_event_loop().run_in_executor(
+                pool, func, *args
+            )
+
+    async def test(self, n: int) -> str:
+        # Calls but gathers aggregated results from coroutine
+        tasks = [self.to_thread(self.fetch_data, i) for i in range(n)]
+        self.res = await asyncio.gather(*tasks)
+
+        if self.debug:
+            print(self.res)
+
+        return 'Done'
+
+    def reference(self, case):
+        self.print_numbers(case)
+
+    async def quantify(self, test_cases, runs=1):
+        sol_start = time.time()
+        for i in range(runs):
+            for case in test_cases:
+                res = await self.test(case)
+                if i == 0:
+                    print(res)
+                else:
+                    res
+        print(f'Runtime for our solution: {time.time() - sol_start}')
+
+        ref_start = time.time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time.time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution(debug=False)
+    test_cases = [10, 9, 8, 7, 6]
+    # Classic way (Python ~3.6) of handling async fetch
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(test.quantify(test_cases))
+    loop.close()
