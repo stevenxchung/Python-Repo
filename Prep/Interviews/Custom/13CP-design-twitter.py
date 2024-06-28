9916f38085d9717939318ab5f03d850df9faeaf2
Steven <stevenxchung@gmail.com>
2024-06-28 09:00:58 -0400

    Create 13CP-design-twitter.py

---
+++ Prep/Interviews/Custom/13CP-design-twitter.py
@@ -0,0 +1,31 @@
+'''
+Original: https://leetcode.com/problems/design-twitter/
+
+Design a simplified version of Twitter where users can:
+- Post tweets
+- Follow and unfollow another user
+- Is able to see the 10 most recent tweets in the user's news feed
+'''
+
+from collections import defaultdict
+from typing import Dict
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def post_tweet(self):
+        return
+
+    def follow(self):
+        return
+
+    def unfollow(self):
+        return
+
+    def get_feed(self):
+        return
+
+
+test = Solution()
